7e3e4e4990ea1158165fc1a68fdc93d92830445f
Chris Jantzen <chrisjantzen@Chriss-MacBook-Pro.local>
2018-03-31 22:31:47 -0400

    work in progress, bot now reads tweets

--- twitterBot/bot.js
+++ twitterBot/bot.js
@@ -1,30 +1,70 @@
-let Twit = require('twit');
-let config = require('./config.js');
+const Twit = require('twit');
+const config = require('./config.js');
+const subs = require('./substitutions.js');
 
-let T = new Twit(config);
+let Twitter = new Twit(config);
 
-let stream = T.stream('user');
+let params = {screen_name: 'CNN'};
+let userDetails = {
+  description: '',
+  screen_name: '',
+  id: 0,
+  count: 1,
+  exclude_replies: true,
+  include_rts: false
+};
 
-stream.on('follow', followed);
+Twitter.get('users/lookup', params, (error, users, response) => {
+  if(error) throw error;
 
-function followed(eventMsg) {
-  console.log("Followed!");
-  let name = eventMsg.source.name;
-  let screenName = eventMsg.source.screen_name;
-  postTweet('@' + screenName + ' thanks for following, ' + name + "!");
-}
+  userDetails.description = users[0].name;
+  userDetails.screen_name = users[0].screen_name;
+  userDetails.id = users[0].id;
 
-function postTweet(txt) {
-  let tweet = {
-    status: txt
-  };
-
-  T.post('statuses/update', tweet, (err, data, response) => {
-    if (err) {
-      console.log("Error has occured");
+  Twitter.get('statuses/user_timeline', userDetails, (error, tweets, response) => {
+    if(!error) {
+      console.log(tweets);
     }
     else {
-      console.log("Tweet Posted");
+      console.log('error');
     }
   });
-}
+});
+
+//TODO: Figure out stream, make modifications to text if possible and post new tweet, setup EC2 instance with this to host the service permanently.
+
+
+
+
+
+
+// stream.on('tweet', (tweet) => {
+//   console.log(tweet);
+// });
+//
+// let stream = Twitter.stream('statuses/sample');
+
+
+
+
+// function followed(eventMsg) {
+//   console.log("Followed!");
+//   let name = eventMsg.source.name;
+//   let screenName = eventMsg.source.screen_name;
+//   postTweet('@' + screenName + ' thanks for following, ' + name + "!");
+// }
+//
+// function postTweet(txt) {
+//   let tweet = {
+//     status: txt
+//   };
+//
+//   T.post('statuses/update', tweet, (err, data, response) => {
+//     if (err) {
+//       console.log("Error has occured");
+//     }
+//     else {
+//       console.log("Tweet Posted");
+//     }
+//   });
+// }
---
+++ twitterBot/substitutions.js
@@ -0,0 +1,16 @@
+let subs = {
+  'witnesses': 'these dudes I know',
+  'allegedly': 'kinda probably',
+  'new study': 'Tumblr post',
+  'rebuild': 'avenge',
+  'space': 'spaaace',
+  'google glass': 'Virtual Boy',
+  'smartphone': 'pokedex',
+  'electronic': 'atomic',
+  'senator': 'Elf-Lord',
+  'car': 'cat',
+  'election': 'eating contest',
+  'congressional leaders': 'river spirits',
+  'homeland security': 'homestar runner',
+  'could not be reached for comment': 'is guilty and everyone knows it'
+}
