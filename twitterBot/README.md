4fe27dd07b1a7eb84b4b44311ab22045cfde2c37
Chris Jantzen <chrisjantzen@Chriss-MacBook-Pro.local>
2018-04-12 21:09:35 -0400

    remove twitterbot

--- twitterBot/README.md
+++
@@ -1,5 +0,0 @@
-## Media meme twitterbot
-
-### Reads new tweets as they appear on the timeline and compares the text to a list of words and alternatives to replace and posts a new tweet if there were any changes to be made.
-
-#### written with nodejs and twit framework
--- twitterBot/config.js
+++
@@ -1,7 +0,0 @@
-// API Credentials
-module.exports = {
-    consumer_key: 'L4ziEzHsSnYZD6ZWlnQnbLL48',
-    consumer_secret: 'c3TzyVDry84NBCZY0Zdp8trhWbAcbu0FCoYfsbaPL1cwwDVmPK',
-    access_token: '965304625707585537-0iHCHwqjNegMavFC5aq2hAHTFeWgZVj',
-    access_token_secret: 'XHAaHPAfyLaXLk1xqNMVB8r13ITp1CO8DW2Lfx7TUDsPt'
-}
